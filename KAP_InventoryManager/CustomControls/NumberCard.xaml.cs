c0a612cf1dddf6502bae64a5b87021ec4cddfabe
Hasini Punsala <hasinipunsala@gmail.com>
2023-12-22 20:42:49 +0530

    feat: add inventory details view

---
+++ KAP_InventoryManager/CustomControls/NumberCard.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KAP_InventoryManager.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for NumberCard.xaml
+    /// </summary>
+    public partial class NumberCard : UserControl
+    {
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(NumberCard));
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(string), typeof(NumberCard));
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(string), typeof(NumberCard));
+
+        public static readonly DependencyProperty BackColorProperty =
+            DependencyProperty.Register("BackColor", typeof(System.Windows.Media.Brush), typeof(NumberCard));
+
+        public static readonly DependencyProperty IconColorProperty =
+            DependencyProperty.Register("IconColor", typeof(System.Windows.Media.Brush), typeof(NumberCard));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        public string Value
+        {
+            get { return (string)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public string Icon
+        {
+            get { return (string)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public System.Windows.Media.Brush BackColor
+        {
+            get { return (System.Windows.Media.Brush)GetValue(BackColorProperty); }
+            set { SetValue(BackColorProperty, value); }
+        }
+        public System.Windows.Media.Brush IconColor
+        {
+            get { return (System.Windows.Media.Brush)GetValue(IconColorProperty); }
+            set { SetValue(IconColorProperty, value); }
+        }
+        public NumberCard()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
---
+++ KAP_InventoryManager/CustomControls/TextCard.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KAP_InventoryManager.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for TextCard.xaml
+    /// </summary>
+    public partial class TextCard : UserControl
+    {
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(TextCard));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public TextCard()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
