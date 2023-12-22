b7ca05e25864f333e61c2fbbfb687fea936b209c
Hasini Punsala <hasinipunsala@gmail.com>
2023-12-22 11:55:46 +0530

    Add button and search bar as custom controls

---
+++ KAP_InventoryManager/CustomControls/Button.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KAP_InventoryManager.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for Button.xaml
+    /// </summary>
+    public partial class Button : UserControl
+    {
+        public static readonly DependencyProperty ButtonTextProperty =
+            DependencyProperty.Register("ButtonText", typeof(string), typeof(Button));
+
+        public static readonly DependencyProperty ButtonIconProperty =
+            DependencyProperty.Register("ButtonIcon", typeof(string), typeof(Button));
+
+        public static readonly DependencyProperty ButtonBackgroundColorProperty =
+            DependencyProperty.Register("ButtonBackgroundColor", typeof(System.Windows.Media.Brush), typeof(Button));
+
+        public string ButtonText
+        {
+            get { return (string)GetValue(ButtonTextProperty); }
+            set { SetValue(ButtonTextProperty, value); }
+        }
+
+        public string ButtonIcon
+        {
+            get { return (string)GetValue(ButtonIconProperty); }
+            set { SetValue(ButtonIconProperty, value); }
+        }
+
+        public System.Windows.Media.Brush ButtonBackgroundColor
+        {
+            get { return (System.Windows.Media.Brush)GetValue(ButtonBackgroundColorProperty); }
+            set { SetValue(ButtonBackgroundColorProperty, value); }
+        }
+
+        // Regular property for the click event handler
+        public RoutedEventHandler ButtonClickHandler { get; set; }
+
+        public Button()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Invoke the dynamically assigned click event handler
+            ButtonClickHandler?.Invoke(sender, e);
+        }
+    }
+}
---
+++ KAP_InventoryManager/CustomControls/SearchBar.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KAP_InventoryManager.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for SearchBar.xaml
+    /// </summary>
+    public partial class SearchBar : UserControl
+    {
+        public static readonly DependencyProperty SearchFunctionProperty = DependencyProperty.Register("SearchFunction", typeof(Action<string>), typeof(SearchBar));
+
+        public Action<string> SearchFunction
+        {
+            get { return (Action<string>)GetValue(SearchFunctionProperty); }
+            set { SetValue(SearchFunctionProperty, value); }
+        }
+
+        public SearchBar()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSearchTextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string searchText = SearchTextBox.Text;
+                SearchFunction?.Invoke(searchText);
+            }
+        }
+    }
+}
