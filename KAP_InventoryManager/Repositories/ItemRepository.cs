487cbed95bd0e6ff496bbc86454406e23097b61e
Hasini Punsala <hasinipunsala@gmail.com>
2023-12-26 19:41:01 +0530

    feat: add the item repository

---
+++ KAP_InventoryManager/Repositories/ItemRepository.cs
@@ -0,0 +1,64 @@
+﻿using KAP_InventoryManager.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAP_InventoryManager.Repositories
+{
+    internal class ItemRepository : RepositoryBase, IItemRepository
+    {
+        public void Add(ItemModel item)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    using (var command = new MySqlCommand("AddItem", connection, transaction))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.Add("@p_PartNo", MySqlDbType.VarChar).Value = item.PartNo;
+                        command.Parameters.Add("@p_OEMNo", MySqlDbType.VarChar).Value = item.OEMNo;
+                        command.Parameters.Add("@p_Description", MySqlDbType.VarChar).Value = item.Description;
+                        command.Parameters.Add("@p_BrandID", MySqlDbType.VarChar).Value = item.BrandID;
+                        command.Parameters.Add("@p_Category", MySqlDbType.VarChar).Value = item.Category;
+                        command.Parameters.Add("@p_SupplierID", MySqlDbType.VarChar).Value = item.SupplierID;
+                        command.Parameters.Add("@p_BuyingPrice", MySqlDbType.Decimal).Value = item.BuyingPrice;
+                        command.Parameters.Add("@p_UnitPrice", MySqlDbType.Decimal).Value = item.UnitPrice;
+
+                        command.ExecuteNonQuery();
+                    }
+
+                    transaction.Commit();
+                }        
+            }
+        }
+
+
+        public void Delete(string partNo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(ItemModel itemModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ItemModel GetByPartNo(string partNo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
